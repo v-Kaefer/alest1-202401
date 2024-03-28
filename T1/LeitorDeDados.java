70fc91900ff34c3972ef3f3c8b8dfddb4998ba3d
v-Kaefer <128633407+v-Kaefer@users.noreply.github.com>
2024-03-28 18:08:20 -0300

    Correções e Leitor de Dados
    - fix README
    - add LeitorDeDados
    - add ListaTest
    - rm ManipulandoCSVs

--- T1/LeitorDeDados.java
+++ T1/LeitorDeDados.java
@@ -1,9 +1,15 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class LeitorDeDados {
-    static String[] meses = {"JAN", "FEV", "MAR", "ABR", "MAI", "JUN", "JUL", "AGO", "SET", "OUT", "NOV", "DEZ"};
+    
+
+    static String[] headerMeses = {"JAN", "FEV", "MAR", "ABR", "MAI", "JUN", "JUL", "AGO", "SET", "OUT", "NOV", "DEZ\n"};
 
 
     public static void getMeses () { // Print do "Header" com os meses
-        for (String mes : meses) {
+        for (String mes : headerMeses) {
             System.out.print("\t" + mes);
         }
     }
@@ -13,8 +19,27 @@ public class LeitorDeDados {
     public static void main(String[] args) {
 
         getMeses();
+        ReadFile();
         
         
-
     }
+
+
+    public static void ReadFile () {
+    try {
+      File myObj = new File("T1/Dados/transacoes_00020.csv");
+      Scanner myReader = new Scanner(myObj);
+      while (myReader.hasNextLine()) {
+        String data = myReader.nextLine();
+        System.out.println("\t"+data);
+      }
+      myReader.close();
+        } catch (FileNotFoundException e) {
+      System.out.println("Erro, arquivo não encontrado.");
+      e.printStackTrace();
+      }
+    }
+
+
 }
+
---
+++ T1/ListaTest.java
@@ -0,0 +1,14 @@
+public class ListaTest {
+    public static void main(String[] args) {
+        ListaLinearEstatica listinha = new ListaLinearEstatica();
+        listinha.adicionar("tomate");
+        listinha.adicionar("cebola");
+        listinha.adicionar("alho");
+        listinha.adicionar("detergente");
+        listinha.adicionar("shampoo");
+        listinha.adicionar("condicionador");
+        listinha.adicionar("refri");
+        listinha.imprimir();
+
+    }    
+}
